79ab9fdf1069dd478e0fd514c08ce5bdb78b0132
OFPD <orioloriolpalacios@outlook.com>
2022-12-25 23:38:52 -0500

    Correción en CVenta. Cambio de "ListaEnlazada" a "CListaEnlazada"

--- ProyectoFarmacia/CVentas.cs
+++ ProyectoFarmacia/CVentas.cs
@@ -3,17 +3,17 @@ namespace ProyectoFarmacia
     public class CVentas{
         private string? _Codigo_Venta;
         private string? _Codigo_Cliente;
-        private ListaEnlazada? _Lista_Productos;
+        private CListaEnlazada? _Lista_Productos;
         private string? _Fecha;
         private int? _Monto;
 
         public string Codigo_Venta { get => _Codigo_Venta; set => _Codigo_Venta = value; }
         public string Codigo_Cliente { get => _Codigo_Cliente; set => _Codigo_Cliente = value; }
-        public ListaEnlazada Lista_Productos { get => _Lista_Productos; set => _Lista_Productos = value; }
+        public CListaEnlazada Lista_Productos { get => _Lista_Productos; set => _Lista_Productos = value; }
         public string Fecha { get => _Fecha; set => _Fecha = value; }
-        public int Monto { get => _Monto; set => _Monto = value; }
+        public int? Monto { get => _Monto; set => _Monto = value; }
 
-        public CVentas(string codigo_Venta, string codigo_cliente, ListaEnlazada lista_Productos, string fecha, int monto)
+        public CVentas(string codigo_Venta, string codigo_cliente, CListaEnlazada lista_Productos, string fecha, int monto)
         {
             Codigo_Venta = codigo_Venta;
             Codigo_Cliente = codigo_cliente;
