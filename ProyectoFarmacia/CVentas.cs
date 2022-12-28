6da826bed828a80fa99eed5c649e52b77b104ca4
Ian Quispe <iancillonn@gmail.com>
2022-12-28 18:55:09 -0500

    Se arregló la lista por fechas

--- ProyectoFarmacia/CVentas.cs
+++ ProyectoFarmacia/CVentas.cs
@@ -66,7 +66,7 @@ namespace ProyectoFarmacia
             string codigo_venta = Console.ReadLine();
             Console.Write("Ingrese el codigo del cliente: ");
             string codigo_cliente = Console.ReadLine();
-            Console.Write("Ingrese el número de productos: ");
+            Console.Write("Ingrese el productos: ");
             string Producto = Console.ReadLine();
             Console.Write("Ingrese la fecha de la venta: ");
             string fecha = Console.ReadLine();
@@ -77,21 +77,34 @@ namespace ProyectoFarmacia
             Venta.Monstrar();
         }
 
-
+        public override void Listar()
+        {
+            CNodoLista? Aux = Node;
+            for (int i = 0; i < Longitud(); i++)
+            {
+                Console.WriteLine("--------------------Venta {0}--------------------", i + 1);
+                ((CVentas)Aux.Element).Monstrar();
+                Aux = Aux.NextNode;
+            }
+        }
         public void ListarFecha()
         {
             Console.WriteLine("Ingrese una fecha");
             string fecha = Console.ReadLine();
+            int a = 0;
             CNodoLista? Aux = Node;
-            Console.WriteLine("------------Ventas {0}--------", ((CVentas)Aux.Element).fecha());
+            Console.WriteLine("------------Ventas {0}--------", fecha);
             for (int i = 0; i < Longitud(); i++)
             {
                 if (((CVentas)Aux.Element).fecha() == fecha)
+                {
+                    a--;
                     ((CVentas)Aux.Element).Monstrar();
-                if (i == Longitud())
-                    Console.WriteLine("No hay ventas en {0}", fecha);
+                }
                 Aux = Aux.NextNode;
             }
+            if (a == 0)
+                Console.WriteLine("No hay ventas en {0}", fecha);
         }
     }
 }
