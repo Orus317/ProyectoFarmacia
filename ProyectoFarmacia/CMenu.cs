103edeb4bedd49906f8cc57fe681d9b44f555160
Oriol Palacios <orioloriolpalacios@outlook.com>
2022-12-28 15:41:12 -0500

    Creación de clase "ValidarFlotante" en CMenu, tiene el mismo funcionamiento que "ValidarEntero"

--- ProyectoFarmacia/CMenu.cs
+++ ProyectoFarmacia/CMenu.cs
@@ -35,6 +35,36 @@ namespace ProyectoFarmacia
             }
             return opcion;
         }
+        public static float ValidarFlotante(string message, float low, float high)
+        {
+            // Modulo para validar un entero entre dos límites
+            string _opcion = Console.ReadLine();
+            float opcion = 0;
+            // Usar una estructura try-catch para poder poder manejar errores de tipo, generalmente cuando
+            // se ingresan letras u otros caracteres 
+            try
+            {
+                // Intentar parsear el string ingresado
+                opcion = float.Parse(_opcion);
+                // si el string es parseado se verifica que esté entre los límites
+                while (opcion > high || opcion < low)
+                {
+                    // mostrar el mensaje de warning en consola
+                    Console.WriteLine(message);
+                    // recibir nuevamente la opcion
+                    _opcion = Console.ReadLine();
+                    // intentar parsear
+                    opcion = float.Parse(_opcion);
+                }
+            }
+            catch (Exception)
+            {
+                // En caso de que el parseo no funcione inmediatamente se mostrará el warn message y se ejecutará el validador nuevamente
+                Console.WriteLine(message);
+                ValidarFlotante(message, low, high);
+            }
+            return opcion;
+        }
         public static void MostrarMenu(CListaEnlazada Clientes, CListaEnlazada Productos, CListaEnlazada Ventas)
         {
             Console.WriteLine("=====================================");
